15670ac5d6c2e11f41031ad3081ff730545efd78
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-04-22 01:37:03 +0300

    Updates

---
+++ precache-manifest.b63e1c7b6072b3d82f8dd0c655410f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0578a218ab5d949060616127a1ac3cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d5bef7a0f0b2e2b9728",
+    "url": "/static/css/main.0a731d0e.chunk.css"
+  },
+  {
+    "revision": "3892204ac45450b8519b",
+    "url": "/static/js/2.b699734f.chunk.js"
+  },
+  {
+    "revision": "6020e2578ce61e153ee5130f44d173fc",
+    "url": "/static/js/2.b699734f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5bef7a0f0b2e2b9728",
+    "url": "/static/js/main.b6222a60.chunk.js"
+  },
+  {
+    "revision": "0257b0d086c047d3f816",
+    "url": "/static/js/runtime-main.d3e5e5dd.js"
+  }
+]);
