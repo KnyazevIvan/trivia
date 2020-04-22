ad717a9c1d29dba518179ed1c079636422561ed5
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-04-22 18:09:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/header/Header';
 import { Menu } from './components/menu/Menu';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import ThemeContainer from './components/theme/ThemeContainer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
  
     <div className="app">
         <Header/>
       <Route exact path='/'  render= {()=> <Menu/>}/>
       <Route exact path='/theme/:id' component={ThemeContainer}/>
     </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
